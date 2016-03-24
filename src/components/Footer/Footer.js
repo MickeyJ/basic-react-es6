df10327b40bd55400d7afa145d813f8dcf3ada42
Mickey <develop.agility@gmail.com>
2016-03-24 16:49:06 -0600

    house keeping

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,8 @@ import React from 'react'
 export default (props) =>{
   const footStyle = {borderTop: '1px solid #ccc'};
   return(
-    <h5 style={footStyle}> &copy; 2015 {props.title}</h5>
+    <h5 style={footStyle}>
+      &copy; 2015 {props.title}
+    </h5>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,8 @@ import React from 'react'
 export default (props) =>{
   const headerStyle = {borderBottom: '1px solid #ccc'};
   return(
-    <h1 style={headerStyle}>{props.title}</h1>
+    <h1 style={headerStyle}>
+      {props.title}
+    </h1>
   )
 }
